17c65cce41b558fcd9e7e307f9b836009d407443
Xin Wu <xinwuint@gmail.com>
2017-10-23 22:38:58 -0400

    localize keyboard title

--- kiosk/assets/localization/localizations.js
+++ kiosk/assets/localization/localizations.js
@@ -1,24 +1,26 @@
 ﻿String.toLocaleString({
 
-	"en": {
-		"attract.start": "START",
-		"attract.desc": "<- To begin session, clike on it.",
-		"attract.videourl": "assets/video/attract.mp4",
-	    "rss.cat.topnews": "TOP NEWS",
-	    "rss.cat.sportsnews": "SPORTS NEWS",
-	    "footer.noconn.title": "network interruption - <br>no internet connection",
-	    "footer.noconn.desc": "we can't send ab email right now. please blahblah",
-	    "videolist.cat.safety": "SAFETY",
-	    "videolist.cat.whyryder": "WHY RYDER",
-	    "keyboard.backtovideo": "BACK TO VIDEO",
-	    "email.title": "Your video site",
-	    "email.body": "Here is the url of videos: {url}.",
-	    "err.title": "NETWORK INTERRUPTOIN - NO INTERNET CONNECTION",
-	    "err.text1": "We can't send an email right now. Please notify a Ryder associate for assistance.",
-	    "err.text2": "Sorry for the inconvenience.",
-	},
+    "en": {
+        "attract.start":            "START",
+        "attract.desc":             "<- To begin session, clike on it.",
+        "attract.videourl":         "assets/video/attract.mp4",
+        "rss.cat.topnews":          "TOP NEWS",
+        "rss.cat.sportsnews":       "SPORTS NEWS",
+        "footer.noconn.title":      "network interruption - <br>no internet connection",
+        "footer.noconn.desc":       "we can't send ab email right now. please blahblah",
+        "videolist.cat.safety":     "SAFETY",
+        "videolist.cat.whyryder":   "WHY RYDER",
+        "keyboard.desc":            "Ryder is committed to confidentiality. A link to this video content will be sent to your email address.",
+        "keyboard.title":           "ENTER EMAIL ADDRESS:",
+        "keyboard.backtovideo":     "BACK TO VIDEO",
+        "email.title":              "Your video site",
+        "email.body":               "Here is the url of videos: {url}.",
+        "err.title":                "NETWORK INTERRUPTOIN - NO INTERNET CONNECTION",
+        "err.text1":                "We can't send an email right now. Please notify a Ryder associate for assistance.",
+        "err.text2":                "Sorry for the inconvenience.",
+    },
 
-	"fr": {
-	}
+    "fr": {
+    }
 
 });
