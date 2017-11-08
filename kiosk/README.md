006910d57e8fc8d77929e603a75ebdae973079ff
Xin Wu <xinwuint@gmail.com>
2017-11-08 12:41:44 -0500

    gulp will create build folder

--- kiosk/README.md
+++ kiosk/README.md
@@ -6,14 +6,11 @@
 - Enter folder 'kiosk'
 - Execute 'npm install'
 
-## Compile scss files
-
-- Execute 'gulp sass' to compile scss files.
-- Execute 'gulp' to execute a gulp's watch daemon that will auto compile (and minify) scss files.
+## Compile and build
 
 All Gulp task can be located at gulpfile.js, and they currently are:
 - 'gulp build:css'      <- Compile/minify scss files one time only.
-- 'gulp build'      	<- Build project (right now only css files).
+- 'gulp build'      	<- Build project and create folder 'build'.
 - 'gulp watch'          <- Watch for modifications and build automatically.
 - 'gulp'                <- Equivalent to 'gulp build'.
 
--- kiosk/gulpfile.js
+++ kiosk/gulpfile.js
@@ -2,17 +2,35 @@
 ;(function(){
 	'use strict';
 
-	var gulp = require('gulp');
-	var sass = require('gulp-sass');
+	var gulp = require('gulp'),
+        sass = require('gulp-sass'),
+        del = require('del');
 
 	gulp.task('build:css', function() {
-	  return gulp.src('assets/sass/**/*.scss')
-	    .pipe(sass()) // Converts Sass to CSS with gulp-sass
-	    .pipe(gulp.dest('assets/css/'))
+        return gulp.src('./assets/sass/**/*.scss')
+            .pipe(sass()) // Converts Sass to CSS with gulp-sass
+            .pipe(gulp.dest('./build/assets/css/'))
+            .pipe(gulp.dest('./assets/css/'));
 	});
 
+    gulp.task('build:other', function () {
+        // build fonts, images, js, localization, html
+        return gulp
+            .src([
+                './index.html',
+                './assets/fonts/**/*',
+                './assets/images/**/*',
+                './assets/js/**/*',
+                './assets/localization/**/*',
+                './config/**/*', 
+                './content/**/*'
+            ], { base: './' })
+            .pipe(gulp.dest('./build'));
+    });
+
 	gulp.task('build', [
-		'build:css'
+		'build:css',
+        'build:other'
 	]);
 
 	gulp.task('watch', ['build'], function() {
@@ -20,5 +38,11 @@
 	});
 
 	gulp.task('default', ['build']);
+
+    gulp.task('clean', function () {
+        return del('./build', function (err, deletedFiles) {
+            console.log('Files deleted : ' + deletedFiles.join(','));
+        });
+    });
 
 })();
