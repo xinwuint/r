5c286bf713f75cad3768bc27d811d408b32a1b34
Xin Wu <xinwuint@gmail.com>
2017-11-09 00:18:43 -0500

    update gulpfile

--- kiosk/gulpfile.js
+++ kiosk/gulpfile.js
@@ -1,48 +1,53 @@
 
 ;(function(){
-	'use strict';
+    'use strict';
 
-	var gulp = require('gulp'),
+    var _src = {
+        scss: './src/assets/sass/**/*.scss',
+        otherassets: [
+            './src/index.html',
+            './src/assets/images/**/*',
+            './src/assets/js/**/*',
+            './src/config/**/*'
+        ],
+        common: '../common/**/*'
+    };
+
+    var gulp = require('gulp'),
         sass = require('gulp-sass'),
         del = require('del');
 
-	gulp.task('build:css', function() {
-        return gulp.src('./src/assets/sass/**/*.scss')
+    gulp.task('build:css', function() {
+        return gulp
+            .src(_src.scss)
             .pipe(sass()) // Converts Sass to CSS with gulp-sass
             .pipe(gulp.dest('./build/assets/css/'));
-	});
+    });
 
     gulp.task('build:other', function () {
         // build fonts, images, js, localization, html
         return gulp
-            .src([
-                './src/index.html',
-                './src/assets/images/**/*',
-                './src/assets/js/**/*',
-                './src/config/**/*', 
-            ], { base: './src' })
+            .src(_src.otherassets, { base: './src' })
             .pipe(gulp.dest('./build'));
     });
 
     gulp.task('build:common', function () {
         return gulp
-            .src([
-                '../common/**/*'
-            ], { base: '../common' })
+            .src(_src.common, { base: '../common' })
             .pipe(gulp.dest('./build'));
     });
 
-	gulp.task('build', [
-		'build:css',
+    gulp.task('build', [
+        'build:css',
         'build:other',
         'build:common'
-	]);
+    ]);
 
-	gulp.task('watch', ['build'], function() {
-	    gulp.watch('./src/assets/sass/**/*.scss', ['build:css']);
-	});
+    gulp.task('watch', ['build'], function() {
+        gulp.watch(_src.scss, ['build:css']);
+    });
 
-	gulp.task('default', ['build']);
+    gulp.task('default', ['build']);
 
     gulp.task('clean', function () {
         return del('./build', function (err, deletedFiles) {
--- videosite/gulpfile.js
+++ videosite/gulpfile.js
@@ -2,30 +2,33 @@
 ;(function(){
 	'use strict';
 
+    var _src = {
+        otherassets: [
+            './src/index.html',
+            './src/assets/images/**/*',
+            './src/js/**/*',
+            './src/css/**/*',
+            '!./src/css/**/*.scss',
+            '!./src/css/**/*.less',
+            '!./src/css/**/*.map',
+            './src/config/**/*'
+        ],
+        common: '../common/**/*'
+    };
+
 	var gulp = require('gulp'),
         sass = require('gulp-sass'),
         del = require('del');
 
     gulp.task('build:other', function () {
         return gulp
-            .src([
-                './src/index.html',
-                './src/assets/images/**/*',
-                './src/js/**/*',
-                './src/css/**/*',
-                '!./src/css/**/*.scss',
-                '!./src/css/**/*.less',
-                '!./src/css/**/*.map',
-                './src/config/**/*'
-            ], { base: './src' })
+            .src(_src.otherassets, { base: './src' })
             .pipe(gulp.dest('./build'));
     });
 
     gulp.task('build:common', function () {
         return gulp
-            .src([
-                '../common/**/*'
-            ], { base: '../common' })
+            .src(_src.common, { base: '../common' })
             .pipe(gulp.dest('./build'));
     });
 
